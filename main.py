4d02e238566782536d486ae72fad2e65c141ee44
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2024-01-04 09:15:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -256,26 +256,8 @@ USAGE = """**FOR PUBLIC CHATS**
 **__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
 then send post/s link__**
 
-**FOR BOT CHATS**
-
-**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
-
-```
-https://t.me/b/botusername/4321
-```
-
-**MULTI POSTS**
-
-**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-**__note that space in between doesn't matter__**
-"""
+**Join Our Channel @Official_InstaPLUS**
+**Owner @HackerPushkar**"""
 
 
 # infinty polling
